45b56b6f8e80d434253e4d6a95b57a5b27b28853
Gavin Mogan <git@gavinmogan.com>
2019-04-20 21:50:41 -0700

    more moving to configuration as code

--- groovy/0007-setup-github-global-config.groovy
+++ groovy/0007-setup-github-global-config.groovy
@@ -3,11 +3,6 @@ import org.jenkinsci.plugins.github.GitHubPlugin
 import org.jenkinsci.plugins.github.config.GitHubPluginConfig;
 import org.jenkinsci.plugins.github.config.GitHubServerConfig;
 
-JenkinsLocationConfiguration jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-jenkinsLocationConfiguration.setUrl("https://jenkins.gavinmogan.com")
-jenkinsLocationConfiguration.setAdminAddress("jenkins@gavinmogan.com)
-jenkinsLocationConfiguration.save()
-
 GitHubPluginConfig pluginConfig = GitHubPlugin.configuration();
 GitHubServerConfig serverConfig = new GitHubServerConfig('github-admin-halkeye')
 pluginConfig.setConfigs([serverConfig])
